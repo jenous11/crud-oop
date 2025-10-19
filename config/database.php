38cb7e3755b2837426852aefd729c297c6062994
jenous11 <jenousdongol11@gmail.com>
2025-10-19 22:09:48 +0545

    added featuers to add things in the databae dynamically

--- config/database.php
+++ config/database.php
@@ -8,15 +8,12 @@ class Database
     //method
     protected function connect()
     {
-        try 
-            {
+        try {
             $pdo = new PDO("mysql:host=" . $this->servername . ";dbname=" . $this->dbname, $this->username, $this->password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo ("connected successfully");
+            // echo ("connected successfully");
             return $pdo;
-            } 
-            catch (PDOException $e) 
-            {
+        } catch (PDOException $e) {
             echo "error " . $e->getMessage();
         }
     }
@@ -25,35 +22,56 @@ class Database
 class Query extends Database
 {
     //method
-    public function getData($table,$fields)
+    public function getData($table, $fields)
     {
         // to get data
-        try 
-        {
+        try {
             $sql = "SELECT $fields FROM $table";
             $stmt = $this->connect()->prepare($sql);
             $stmt->execute();
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            print_r($result);
             return $result;
-        } catch (PDOException $e)
-            {
+        } catch (PDOException $e) {
             echo "error: " . $e->getMessage();
         }
     }
     //function to insert data
-    public function insertData($table)
+    public function insertData($table, $param)
     {
-        try
-        {
-        $sql = "INSERT INTO $table (`name`, email,phone ) VALUES ('jenous','jenousdongol11@gmail.com','9818084977');";
-        $stmt= $this->connect()->prepare($sql);
-        $stmt->execute();
-        $stmt=null;
+        try {
+        // echo"<pre>";
+        // print_r($param);
+        $fields=array();
+        $values=array();
+
+        foreach($param as $key=>$value){
+            $fields[]=$key;
+            $values[]=$value;
+        }
+        // print_r($fields);
+        // print_r($values);
+
+        $fields=implode("," , $fields);
+        $values=implode("','" , $values);
+        $values ="'" . $values . "'";
+        exit;
+
+
+        //this shows values in the screen
+        //    echo "<pre>";
+        // echo "Fields: $fields\n";
+        // echo "Values: $values\n";
+        // echo "</pre>";
+
+           echo $sql = "INSERT INTO $table ($fields) 
+           VALUES ($values);";
+        //    exit; 
+
+           $stmt = $this->connect()->prepare($sql);
+            $stmt->execute();
+            // $stmt = null;
+        } catch (PDOException $e) {
+            echo "error: " . $e->getMessage();
+        }
     }
-    catch(PDOException $e)
-    {
-        echo "error: ".$e->getMessage();
-    }
-}
 }
--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
 require 'config/Database.php';
 $obj = new Query();
+
+
+//making and associate array to insert data dynamically
+$data = array(
+    'name' =>'Amit',
+    'email' => "amit@gmail.com",
+    'phone'=>"9818898989" 
+);
+
+//insert data
+$obj->insertData('users',$data);
+
+//get and show data
 $obj->getData('users','*');
 print_r($obj->getData('users','*'));
 
-$obj->insertData('users');
+
+// $obj->result;
 ?>
 
 
